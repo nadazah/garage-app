259fa8f5f081ec9848d5abaf91b295371b17622b
nadazah <zahranada456@gmail.com>
2023-11-01 23:32:32 +0100

    first commit

---
+++ src/main/java/com/spring/Garageapp/GarageappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Garageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/Garageapp/GarageappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Garageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
